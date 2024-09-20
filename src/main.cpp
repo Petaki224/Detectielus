4bdb4c5bc1a94638b944328447b6d1af02ef1d42
Evius <90718819+Petaki224@users.noreply.github.com>
2024-09-20 10:37:15 +0200

    opdracht en comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <Arduino.h>
-// Als je dit leest schreeuw frikandelbroodje
+
+// Gebruik #define of const voor fixed values
+//gebruik juiste pin benamingen!!!
+//gebruik functies!!!
+//gebruik juiste datatypes!!!
+// Duidelijke en voldoende hoeveelheid Comments toevoegen!!!!
 
 int main(void){
-
+  //main() conform ontwerp
 
   return 0;
+}
+
+
+void display_counter(){
+  //functie display_counter() met juiste oneliner om aantal gepasseerde voertuigen weer te geven.
+}
+
+void button_state(){
+  //functie button_state() waarin debouncing wordt gedaan
+}
+
+void vehicle_passed(){
+  //functie vehicle_passed() geeft de juiste boolean waarde
+}
+
+void init_pins(){
+  //functie init_pins() initialiseert de pinnen juist
 }
